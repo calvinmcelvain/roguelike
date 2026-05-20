172e0a5b4137a0746f69a7a420984b0a9680c89f
calvin-mcelvain <mcelvain1@hotmail.com>
2026-05-19 21:07:03 -0400

    feat: Allowing only 1 enemy directional move per turn & adding noise
    Previously allowed enemies to move in both directions per frame. Now only one direction per frame & making it a 25% chance of no move at all for a given frame.

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstdlib>
 
 Enemy::Enemy(int x, int y, char symbol)
     : position(x, y), symbol(symbol), health(30), attackDamage(5) {}
@@ -9,16 +10,33 @@ Enemy::Enemy(int x, int y, char symbol)
 void Enemy::moveTo(Vector2D newPos) { position = newPos; }
 
 void Enemy::moveTowardPlayer(Vector2D playerPos) {
-  // Simple AI: move one step toward player
-  if (position.x < playerPos.x)
-    position.x++;
-  else if (position.x > playerPos.x)
-    position.x--;
+  // 25% chance to to not move.
+  bool noMove = (std::rand() % 4) == 3;
 
-  if (position.y < playerPos.y)
-    position.y++;
-  else if (position.y > playerPos.y)
-    position.y--;
+  // make move toward player random in direction choice.
+  int dir = std::rand() % 2;
+
+  if (!noMove) {
+    switch (dir) {
+      // vertical dir.
+      case 0:
+        if (position.y < playerPos.y) {
+          position.y++;
+        } else if (position.y > playerPos.y) {
+          position.y--;
+        }
+        break;
+
+      // horizontal dir.
+      case 1:
+        if (position.x < playerPos.x) {
+          position.x++;
+        } else if (position.x > playerPos.x) {
+          position.x--;
+        }
+        break;
+    }
+  }
 }
 
 void Enemy::takeDamage(int damage) { health -= damage; }
