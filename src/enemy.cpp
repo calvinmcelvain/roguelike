8b65d46fc085b303dc76299aaf2a2759a12dd770
calvin-mcelvain <mcelvain1@hotmail.com>
2026-05-24 12:41:31 -0400

    refactor: Updating Enemy.moveTowardPlayer method to move using move hook & randomly drawing a MOVABLE direction
    Previously, we had it where the enemy would NOT move w/ probability of 25% & that if they could move, the direction was random. The former is no longer needed because of the new speed attribute, but the latter was adding another random element... What if the randomly drawn direction was a direction the enemy didnt need to move? They didn't...

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -4,34 +4,30 @@
 #include <cmath>
 #include <cstdlib>
 
+#include "vector2d.h"
+
 void Enemy::moveTowardPlayer(Vector2D playerPos) {
-  // 25% chance to to not move.
-  bool noMove = (std::rand() % 4) == 3;
+  bool needsX = position.x != playerPos.x;
+  bool needsY = position.y != playerPos.y;
 
-  // make move toward player random in direction choice.
-  int dir = std::rand() % 2;
+  int newX = position.x + (position.x < playerPos.x ? 1 : -1);
+  int newY = position.y + (position.y < playerPos.y ? 1 : -1);
 
-  if (!noMove) {
-    switch (dir) {
-      // vertical dir.
-      case 0:
-        if (position.y < playerPos.y) {
-          position.y++;
-        } else if (position.y > playerPos.y) {
-          position.y--;
-        }
-        break;
-
-      // horizontal dir.
-      case 1:
-        if (position.x < playerPos.x) {
-          position.x++;
-        } else if (position.x > playerPos.x) {
-          position.x--;
-        }
-        break;
+  Vector2D newPos = position;
+  if (needsX && needsY) {
+    // only randomly choose when both movements can be made.
+    if (std::rand() % 2 == 0) {
+      newPos.y = newY;
+    } else {
+      newPos.x = newX;
     }
+  } else if (needsX) {
+    newPos.x = newX;
+  } else if (needsY) {
+    newPos.y = newY;
   }
+
+  moveHook(newPos);
 }
 
 void Enemy::takeDamage(int damage) { health -= damage; }
