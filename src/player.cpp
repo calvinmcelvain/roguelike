2e91dbfab11d1b683dd9f569ea2351ba0cdbaa34
calvin-mcelvain <mcelvain1@hotmail.com>
2026-05-24 12:42:17 -0400

    refactor: Calling moveHook for Player move methods
    I actually don't think this is the best implementation... but whatever.

--- src/player.cpp
+++ src/player.cpp
@@ -1,14 +1,14 @@
 #include "player.h"
 
-#include "level.h"
+#include "vector2d.h"
 
-void Player::moveUp() { position.y--; }
+void Player::moveUp() { moveHook(Vector2D(position.x, position.y - 1)); }
 
-void Player::moveDown() { position.y++; }
+void Player::moveDown() { moveHook(Vector2D(position.x, position.y + 1)); }
 
-void Player::moveLeft() { position.x--; }
+void Player::moveLeft() { moveHook(Vector2D(position.x - 1, position.y)); }
 
-void Player::moveRight() { position.x++; }
+void Player::moveRight() { moveHook(Vector2D(position.x + 1, position.y)); }
 
 void Player::takeDamage(int damage) {
   health -= damage;
