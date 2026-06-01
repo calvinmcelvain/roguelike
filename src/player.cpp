297f12478b8c14c498eba994f7ff0449c23ecfcd
calvin-mcelvain <mcelvain1@hotmail.com>
2026-06-01 08:07:41 -0400

    refactor: Removing previous movement methods from Player, and fixing default Player::speed

--- src/player.cpp
+++ src/player.cpp
@@ -2,14 +2,6 @@
 
 #include "coordinate.h"
 
-void Player::moveUp() { moveHook(Coordinate(position.x, position.y - 1)); }
-
-void Player::moveDown() { moveHook(Coordinate(position.x, position.y + 1)); }
-
-void Player::moveLeft() { moveHook(Coordinate(position.x - 1, position.y)); }
-
-void Player::moveRight() { moveHook(Coordinate(position.x + 1, position.y)); }
-
 void Player::takeDamage(int damage) {
   health -= damage;
   if (health < 0) health = 0;
