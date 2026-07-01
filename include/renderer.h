ffb144e0ee4ff51f75e83e30ed22444914b964da
calvin-mcelvain <mcelvain1@hotmail.com>
2026-07-01 09:13:06 -0400

    feat: Creating Renderer
    No initializing args for now. Game object will be composed of a Renderer. Could probably just create a method in Game, but this felt better.

---
+++ include/renderer.h
@@ -0,0 +1,34 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <map>
+
+#include "render_stack.h"
+
+class Renderer {
+ public:
+  /**
+   * @brief Add a rendering layer at a given an index (z-order).
+   *
+   * Layers are composited in ascending key order: lower index = closer to
+   * the bottom.
+   *
+   * NOTE: if layer is added on same index as another layer, the current layer
+   * is replaced with the new one.
+   *
+   * @param z Z-order index.
+   * @param layer RenderStack layer to add.
+   */
+  void addLayer(int z, std::unique_ptr<RenderStack> layer);
+
+  /**
+   * @brief Composite all enabled layers onto stdscr and flush to terminal.
+   *
+   */
+  void compose();
+
+ private:
+  std::map<int, std::unique_ptr<RenderStack>> layers;
+};
+
+#endif
